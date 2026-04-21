579cdb2571096624491955d52a285b68a0254cea
shoby0 <sharoon@statisticsguyana.gov.gy>
2026-04-21 16:57:26 -0400

    Add imputation script
    Initial script for data imputation with documentation.

---
+++ scripts/04_impute.R
@@ -0,0 +1,6 @@
+# 04_impute.R
+# Purpose: Apply basic imputation to cleaned dataset
+# Methods used: mean, median, mode, hot deck or flag
+# Justification for each variable documented here
+# Author: Shoaib
+# Date: April 2026
